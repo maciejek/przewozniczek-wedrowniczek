016e10da3a229b5b943cd7905bee0177d3c1f4df
maciejek <radko.m@hotmail.com>
2014-05-25 14:53:56 +0200

    PromileCalculator added

---
+++ PrzewozniczekWedrowniczek/Alcohol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzewozniczekWedrowniczek
+{
+    public class Alcohol
+    {
+        private readonly double Alcohol_density = 0.79;
+        private double sizeInMl;
+        private double percent;
+
+        public Alcohol(double sizeInL, double percent)
+        {
+            this.sizeInMl = sizeInL * 1000;
+            this.percent = percent;
+        }
+
+        public double CalculateGramsOfAlcohol()
+        {
+            return sizeInMl * percent * Alcohol_density / 100;
+        }
+    }
+}
